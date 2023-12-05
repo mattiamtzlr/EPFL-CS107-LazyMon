0ceec5315bcfbe25ef6bfac2ed0e62bf5e3d7aaf
leolucabernardi <83414821+leolucabernardi@users.noreply.github.com>
2023-12-05 15:46:52 +0100

    working up to 3.2, ! TOWN IS TEST AREA !

--- icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/Town.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/Town.java
@@ -2,11 +2,15 @@ package ch.epfl.cs107.icmon.area.maps;
 
 import ch.epfl.cs107.icmon.actor.items.ICBall;
 import ch.epfl.cs107.icmon.area.ICMonArea;
+import ch.epfl.cs107.icmon.gamelogic.actions.LogAction;
+import ch.epfl.cs107.icmon.gamelogic.events.CollectItemEvent;
+import ch.epfl.cs107.icmon.gamelogic.events.ICMonEvent;
 import ch.epfl.cs107.play.engine.actor.Background;
 import ch.epfl.cs107.play.engine.actor.Foreground;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 
 public class Town extends ICMonArea {
+    private ICMonEvent event;
     @Override
     public DiscreteCoordinates getPlayerSpawnPosition() {
         return new DiscreteCoordinates(5, 5);
@@ -16,7 +20,20 @@ public class Town extends ICMonArea {
     protected void createArea() {
         registerActor(new Background(this));
         registerActor(new Foreground(this));
-        registerActor(new ICBall(this, new DiscreteCoordinates(6, 6), "items/icball"));
+        ICBall ball = new ICBall(this, new DiscreteCoordinates(6, 6), "items/icball");
+        registerActor(ball);
+        event = new CollectItemEvent(ball);
+        event.onStart(new LogAction("Try to collect a ball"));
+        event.onComplete(new LogAction("You collected a ball!"));
+        event.start();
+
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        event.update(deltaTime);
     }
 
     @Override
--- icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/events/ICMonEvent.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/events/ICMonEvent.java
@@ -6,25 +6,70 @@ import ch.epfl.cs107.play.engine.Updatable;
 import java.util.ArrayList;
 
 public class ICMonEvent implements Updatable {
-    private boolean isStarted = false;
-    private boolean isSuspended = false;
-    private boolean isCompleted = false;
+    private boolean started = false;
+    private boolean suspended = false;
+    private boolean completed = false;
 
     private ArrayList<Action> startActions = new ArrayList<>();
     private ArrayList<Action> completeActions = new ArrayList<>();
     private ArrayList<Action> suspendActions = new ArrayList<>();
     private ArrayList<Action> resumeActions = new ArrayList<>();
 
-    void start(){};
-    void complete(){};
-    void suspend(){};
-    void resume(){};
-    void onStart(){};
-    void onComplete(){};
-    void onSuspension(){};
-    void onResume(){};
-    @Override
-    public void update(float deltaTime) {
+    public final void start(){
+        if (!started) {
+            performActions(startActions);
+            started = true;
+        }
+    }
+    public final void complete(){
+        if (!completed && started) {
+            performActions(completeActions);
+            completed = true;
+        }
+    }
+    public final void suspend(){
+        if (!completed && !suspended && started) {
+            performActions(suspendActions);
+            suspended = true;
+        }
+    }
+    public final void resume(){
+        if (suspended && !completed && started) {
+            performActions(resumeActions);
+            suspended = false;
+        }
+    }
+    public final void onStart(Action action){
+        startActions.add(action);
+    }
+    public final void onComplete(Action action){
+        completeActions.add(action);
+    }
+    public final void onSuspension(Action action){
+        suspendActions.add(action);
+    }
+    public final void onResume(Action action){
+        resumeActions.add(action);
+    }
 
+    public boolean isStarted() {
+        return started;
+    }
+
+    public boolean isSuspended() {
+        return suspended;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    @Override
+    public void update(float deltaTime) {}
+
+    private static void performActions(ArrayList<Action> actions){
+        for (Action action : actions) {
+            action.perform();
+        }
     }
 }
