cbd93b92d1ff01ad49e67ad7303f03311e075072
Mattia <mattia.metzler@gmail.com>
2023-12-09 11:59:24 +0100

    Battle Logic with Finite State Machines (4.4)

--- icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/fights/ICMonFight.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/fights/ICMonFight.java
@@ -5,18 +5,24 @@ import ch.epfl.cs107.icmon.graphics.ICMonFightArenaGraphics;
 import ch.epfl.cs107.icmon.graphics.ICMonFightTextGraphics;
 import ch.epfl.cs107.play.engine.PauseMenu;
 import ch.epfl.cs107.play.window.Canvas;
+import ch.epfl.cs107.play.window.Keyboard;
 
 public class ICMonFight extends PauseMenu {
+    private enum ICMonFightState {
+        INTRODUCTION, COUNTER_HANDLING, CONCLUSION
+    }
     private Pokemon player;
     private Pokemon foe;
     private ICMonFightArenaGraphics arena;
-    private float pauseTime = 3.0f;
+    private float pauseTime = 5.0f;
+    private boolean running = true;
+    private ICMonFightState currentState;
 
     public ICMonFight(Pokemon player, Pokemon foe) {
         this.player = player;
         this.foe = foe;
         this.arena = new ICMonFightArenaGraphics(CAMERA_SCALE_FACTOR, player.properties(), foe.properties());
-        this.arena.setInteractionGraphics(new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "UwU"));
+        this.currentState = ICMonFightState.INTRODUCTION;
     }
 
     @Override
@@ -27,9 +33,37 @@ public class ICMonFight extends PauseMenu {
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        pauseTime -= deltaTime;
+
+        switch (this.currentState) {
+            case INTRODUCTION -> {
+                this.arena.setInteractionGraphics(
+                        new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "Start by pressing space-bar"));
+
+                if (getKeyboard().get(Keyboard.SPACE).isPressed())
+                    this.currentState = ICMonFightState.COUNTER_HANDLING;
+            }
+            case COUNTER_HANDLING -> {
+                if (this.pauseTime <= 0)
+                    this.currentState = ICMonFightState.CONCLUSION;
+
+                this.arena.setInteractionGraphics(
+                        new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "Fighting"));
+
+                this.pauseTime -= deltaTime;
+                System.out.println(pauseTime);
+            }
+            case CONCLUSION -> {
+                this.arena.setInteractionGraphics(
+                        new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "Good Fight! End by pressing space-bar"));
+
+                if (getKeyboard().get(Keyboard.SPACE).isPressed()) {
+                    end();
+                    this.running = false;
+                }
+            }
+        }
     }
     public boolean isRunning(){
-        return (pauseTime > 0); // as long as this returns true, the game is paused
+        return running; // as long as this returns true, the game is paused
     }
 }
